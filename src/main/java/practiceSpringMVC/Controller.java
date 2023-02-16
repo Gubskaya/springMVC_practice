bb0fc6aa71fa27810e6de48eecdedf0f750ededc
daria <gybskaya.dd@gmail.com>
2023-02-16 19:40:57 +0600

    first commit

---
+++ src/main/java/practiceSpringMVC/Controller.java
@@ -0,0 +1,11 @@
+package practiceSpringMVC;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello";
+    }
+}
